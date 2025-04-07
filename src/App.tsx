67572eec56abc9cee4427547e9f71279a20b067d
Chew Meng Ghim, Javier <168629385+Vajeri@users.noreply.github.com>
2025-04-07 17:46:14 +0800

    Update App.tsx
    changes:
    
    added:
    import ProtectedRoute from "./components/ProtectedRoute";
    import { AuthProvider } from "./contexts/AuthContext";
    
    updated function App()

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Route */}
+          <Route path="/" element={<LoginPage />} />
+
+          {/* Protected Route */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
